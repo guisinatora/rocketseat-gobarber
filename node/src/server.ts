55591e710155627cc171d2f5a8efa76c147976b4
Guilherme <guisinatora@hotmail.com>
2021-03-07 16:44:37 -0300

    Iniciando o projeto

---
+++ node/src/server.ts
@@ -0,0 +1,12 @@
+/* eslint-disable no-console */
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World!' });
+});
+
+app.listen(3333, () => {
+  console.log('🚀️ Server UP!');
+});
